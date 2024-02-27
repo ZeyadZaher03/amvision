a362ff93e88e60fb0bb99dd31a008071aa7997a7
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-02-27 20:34:01 +0200

    service item

---
+++ src/components/service-item/service-item.tsx
@@ -0,0 +1,34 @@
+import React, { Children } from "react";
+
+import Image, { StaticImageData } from "next/image";
+import styles from "./service-item.module.css";
+
+interface IServiceItem {
+  imgSrc: StaticImageData;
+  imgAlt: string;
+  title: string;
+  children: JSX.Element | JSX.Element[];
+}
+
+export const ServiceItem = ({
+  imgSrc,
+  imgAlt,
+  title,
+  children,
+}: IServiceItem) => {
+  return (
+    <section className={styles.services_item}>
+      <div className={styles.services_item_header}>
+        <div className={styles.services_item_icon_wrapper}>
+          <Image
+            src={imgSrc}
+            alt={imgAlt}
+            className={styles.services_item_icon}
+          />
+        </div>
+        <h4 className={styles.services_item_heading}>{title}.</h4>
+      </div>
+      <p className={styles.services_item_body}>{children}</p>
+    </section>
+  );
+};
