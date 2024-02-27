991f7072590c1d938a18762442167327ed6efd14
ZeyadZaher03 <zeyadzaher02@gmail.com>
2024-02-27 12:48:05 +0200

    remove unnessecry data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { Inter, Roboto } from "next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.className} ${roboto.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
